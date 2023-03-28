51e0ae272d6b75254b49ed34efd7d62d9e741200
Pedro Henrique de Souza <phsouzaa2@gmail.com>
2023-03-28 15:49:21 -0300

    Adicionando classes de Banco de dados, dataset e classe descrevendo o parâmetro

---
+++ entity/parameter.py
@@ -0,0 +1,8 @@
+class parameter:
+    def __init__(self, id, name, type, minValue, maxValue, samplingRate):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.samplingRate = samplingRate
